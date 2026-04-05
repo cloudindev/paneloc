50c489418836796d86771ae97993707507c33e53
muyfresh <muyfreshbos@gmail.com>
2026-04-05 21:09:14 +0200

    chore: remove minio setup test scripts

--- check-minio.js
+++
@@ -1,8 +0,0 @@
-const assert = require("assert");
-try {
-  const Minio = require("minio");
-  console.log(Object.keys(Minio));
-  console.log("Client properties:", Object.getOwnPropertyNames(Minio.Client.prototype));
-} catch(e) {
-  console.log("Minio not installed", e.message);
-}
